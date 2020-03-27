67420734aee5d0344d6fcba3090f99903a3da2e0
ethan <ethan@pip-jr.zhatt.com>
2020-03-26 22:05:42 -0700

    Finished with cp4 (no extra credit)

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -6,23 +6,32 @@ public class FormulaCell extends RealCell {
 	public FormulaCell (String inputText) {
 		fullText = inputText;
 		abbrText = fullText;
-//		String tempVar = inputText.substring(0, inputText.length());
-//		String[] splitEquation = tempVar.split(" ");
-//		for (int i = 0; i < splitEquation.length; i+=2) {
-//			if ()
-//		}
-//		
 	}
-	
 	public String abbreviatedCellText() {
+		abbrText = getDoubleValue() + "          ";
+		abbrText = abbrText.substring(0,10);
 		return abbrText;
 	}
 	
 	public String fullCellText() {
 		return fullText;
 	}
-	
+
 	public double getDoubleValue() {
-		return 1;
+		String tempVar = fullText.substring(2, fullText.length()-2);
+		String[] splitEquation = tempVar.split(" ");
+		double total = Double.parseDouble(splitEquation[0]);
+		for (int i = 1; i < splitEquation.length; i += 2) {
+			if (splitEquation[i].equals("*")) {
+				total *= Double.parseDouble(splitEquation[i + 1]);
+			} else if (splitEquation[i].equals("/")) {
+				total /= Double.parseDouble(splitEquation[i + 1]);
+			} else if (splitEquation[i].equals("+")) {
+				total += Double.parseDouble(splitEquation[i + 1]);
+			} else if (splitEquation[i].equals("-")) {
+				total -= Double.parseDouble(splitEquation[i + 1]);
+			}
+		}
+		return total;
 	}
 }
